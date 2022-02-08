fad99917b33587412a38929f638e0f3ca2df5ace
bernardn38 <erisboxx@gmail.com>
2022-02-07 21:07:18 -0600

    update

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -3,22 +3,21 @@ import "../css/carousel.css";
 import { Carousel } from "react-bootstrap";
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
-  const [items, setItems] = useState([
+  const [items, setItems] = useState();
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  setItems([
     {
       src: "https://cdn.vox-cdn.com/thumbor/ew8i4npv71ZawLACdH245qK0VE8=/0x0:5174x3449/1200x800/filters:focal(2174x1312:3000x2138)/cdn.vox-cdn.com/uploads/chorus_image/image/67086155/shutterstock_1497472160.0.jpg",
     },
     {
-      src: "https://media.istockphoto.com/photos/pumpkin-punch-or-sangria-in-a-glass-with-apple-cinnamon-halloween-and-picture-id1301587633?s=612x612",
+      src: "https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1914&q=80",
     },
     {
       src: "https://media.istockphoto.com/photos/table-with-various-cookies-tarts-cakes-cupcakes-and-cakepops-picture-id623709028?s=612x612",
     },
-  ]);
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-
+  ])
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}   >
       {items.map((item) => {
